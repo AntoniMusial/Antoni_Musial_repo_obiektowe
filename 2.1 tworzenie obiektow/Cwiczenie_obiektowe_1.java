b89cf1a46c66c6dd2ae76e2e0371c784aa08c78b
AntoniMusial <>
2022-03-30 09:34:41 +0200

    aktualizuje pliki z zadania

--- 2.1 tworzenie obiektow/Cwiczenie_obiektowe_1.java
+++ 2.1 tworzenie obiektow/Cwiczenie_obiektowe_1.java
@@ -1,5 +1,6 @@
 public class Cwiczenie_obiektowe_1 {
     public static void main(String[] args) {
+
         System.out.println("hej");
     }
 }
---
+++ 2.1 tworzenie obiektow/MojeDane.java
@@ -0,0 +1,5 @@
+public class MojeDane {
+    public static void main(String[] args) {
+
+    }
+}
