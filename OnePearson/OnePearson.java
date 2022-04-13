3df38263e5ee0e9daf158f1e109e7f7448b4fc63
AntoniMusial <>
2022-04-13 09:32:24 +0200

    aktualizuje plik z zadania

---
+++ OnePearson/OnePearson.java
@@ -0,0 +1,19 @@
+public class OnePearson {
+    private String name;
+    private String last_name;
+    private int earn;
+
+    public OnePearson(String name, String last_name, int earn) {
+        this.name = name;
+        this.last_name = last_name;
+        this.earn = earn;
+    }
+
+    public void readFromCSV(File file, String separator) throws FileNotFoungExpection {
+        Scanner sc = new Scanner(file);
+        while (sc.hasNext()) {
+            String[tab] = sc.nextLine().split(separator);
+            showTableContent(tab);
+        }
+    }
+}
