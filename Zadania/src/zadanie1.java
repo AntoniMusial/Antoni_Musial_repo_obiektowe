1a580a7898c6ccb5247b82261bff3ddc7c3cf170
AntoniMusial <>
2022-06-01 09:14:23 +0200

    dodaje zadanie nie skonczone 2

---
+++ Zadania/src/zadanie1.java
@@ -0,0 +1,22 @@
+import org.codehaus.groovy.runtime.powerassert.SourceText;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
+
+public class zadanie1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> losoweliczby = new ArrayList<Integer>();
+        for (int i=1;i<100;i++) {
+            losoweliczby.add(i);
+        }
+        for (int i = 1; i < losoweliczby.size(); i++) {
+            int index = (int)(Math.random() * losoweliczby.size());
+
+            System.out.println("(" + i + ") Losowa liczba: " + losoweliczby.get(index));
+        }
+        System.out.println("Największa wartość: " + Collections.max(losoweliczby));
+        System.out.println("Najmniejsza wartość: " + Collections.min(losoweliczby));
+    }
+}
