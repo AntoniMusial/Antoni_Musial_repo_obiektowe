744d70180245c6a24201992362ba478ed069431f
AntoniMusial <>
2022-10-17 11:35:41 +0200

    dodaje kolejna kupekupe

---
+++ 3KLasa/Pola statyczne 0_7_1/Numbers.java
@@ -0,0 +1,11 @@
+public class Numbers {
+    private static int l1 = 2;
+    private int l2;
+
+
+    public static void main(String[] args) {
+        l2 = new Integer();
+        l2 = 68;
+        System.out.println(l2);
+    }
+}
---
+++ 3KLasa/Pola statyczne 0_7_1/Pola_statyczne.java
@@ -0,0 +1,5 @@
+public class Pola_statyczne {
+    public static void main(String[] args) {
+        System.out.println("dupa");
+    }
+}
