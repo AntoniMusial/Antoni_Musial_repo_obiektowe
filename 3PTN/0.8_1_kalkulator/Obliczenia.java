e207ee87d6a252b6e95379a1f8011a9eb7b24e46
AntoniMusial <>
2022-11-21 11:52:07 +0100

    jakies kurwa gowno dodaje japierdole ale pompa JUHU

---
+++ 3PTN/0.8_1_kalkulator/Obliczenia.java
@@ -0,0 +1,64 @@
+public class Obliczenia {
+    private int l1, l2;
+ 
+    // Setter
+    public void setL1(Integer newl1) {
+      this.l1 = newl1;
+    }
+
+    public static int getL1() {
+        return l2;
+    }
+    
+    public static int getL2() {
+        return l2;
+    }
+
+    public static void setL2(int l2) {
+        Main.l2 = l2;
+    }
+
+    //MNOŻENIE
+    public int mnozenie() {
+        System.out.println("mnozenie");
+        return (l1 * l2);
+    }
+
+    //DZIELENIE
+    int dzielenie() {
+        System.out.println("dzielenie");
+        return 0;
+    }
+    
+    //DODAWANIE
+    int dodawanie() {
+        System.out.println("dodawanie");
+        return 0;
+    }
+    
+    //ODEJMOWANIE
+    int odejmowanie() {
+        System.out.println("odejmowanie");
+        return 0;
+    }
+    
+    //POTĘGOWANIE
+    int potegowanie() {
+        System.out.println("potegowanie");
+        return 0;
+    }
+    
+    //PIERWIASTKOWANIE
+    int pierwiastkowanie() {
+        System.out.println("pierwiastkowanie");
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        final int getL1() {
+            return l1;
+        }
+
+        System.out.println(l1);
+    }
+}
