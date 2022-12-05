230c7dc7925085cc1eee6f3c723a8c79d7ad1e1f
Antoni <antoni.glowny@gmail.com>
2022-12-05 20:36:36 +0100

    s

--- 3PTN/Pola statyczne 0_7_1/Numbers.java
+++ 3PTN/Pola statyczne 0_7_1/Numbers.java
@@ -1,65 +1,48 @@
-<<<<<<< HEAD
 public class Numbers {
-    private Numbers l1;
-    public Numbers getL1() {
+    private static int l1 = 2;
+    private int l2;
+
+    public void setL1(int l1) {
+        this.l1 = l1;
+    }
+
+    public void setL2(int l2) {
+        this.l2 = l2;
+    }
+
+    public Integer getL1() {
         return l1;
     }
-    public void setL1(Numbers l1) {
-        this.l1 = l1;
-    }
 
-    private Numbers l2;
-    public Numbers getL2() {
+    public Integer getL2() {
         return l2;
     }
-    public void setL2(Numbers l2) {
-        this.l2 = l2;
+
+    public Numbers() {
+        l2 = 4;
     }
 
-    public Numbers(Numbers l1, Numbers l2) {
-        this.l1 = l1;
-        this.l2 = l2;
-    }
-
-    static void showMultiply(Numbers l1, Numbers l2) {
-        int sum = 
-        System.out.println(l1 * l2);
+    public void showMiltiply() {
+        System.out.println(l1 + " * " + l2 + " = " + (l1 * l2));
     }
 
     public static void main(String[] args) {
-        showMultiply();
+        Numbers numbers = new Numbers();
+        numbers.l2 = 4;
+        numbers.showMiltiply();
+        Numbers n1 = new Numbers();
+        Numbers n2 = new Numbers();
+        Numbers n3 = new Numbers();
+        Numbers n4 = new Numbers();
+        Numbers n5 = new Numbers();
+        Numbers n6 = new Numbers();
+        Numbers n7 = new Numbers();
+        Numbers n8 = new Numbers();
+        Numbers n9 = new Numbers();
+        Numbers n10 = new Numbers();
+        Numbers numbers1 = Numbers(1);
+
+        System.out.println(numbers.l2);
+        System.out.println(numbers.getL1());
     }
 }
-=======
-public class Numbers {
-    private Numbers l1;
-    public Numbers getL1() {
-        return l1;
-    }
-    public void setL1(Numbers l1) {
-        this.l1 = l1;
-    }
-
-    private Numbers l2;
-    public Numbers getL2() {
-        return l2;
-    }
-    public void setL2(Numbers l2) {
-        this.l2 = l2;
-    }
-
-    public Numbers(Numbers l1, Numbers l2) {
-        this.l1 = l1;
-        this.l2 = l2;
-    }
-
-    static void showMultiply(Numbers l1, Numbers l2) {
-        int sum = 
-        System.out.println(l1 * l2);
-    }
-
-    public static void main(String[] args) {
-        showMultiply();
-    }
-}
->>>>>>> ea21080d8aff0abdb7115f5eb12c9674056523d8
